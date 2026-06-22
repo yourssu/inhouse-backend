9aa5ae8c0ec43a44e394cd9229144de62dfa2a45
leeminseo <leeminseo0923@gmail.com>
2026-06-22 14:43:47 +0900

    initialize commit

---
+++ src/main/kotlin/com/yourssu/inhouse/InhouseApplication.kt
@@ -0,0 +1,11 @@
+package com.yourssu.inhouse
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InhouseApplication
+
+fun main(args: Array<String>) {
+    runApplication<InhouseApplication>(*args)
+}
---
+++ src/test/kotlin/com/yourssu/inhouse/InhouseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yourssu.inhouse
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InhouseApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
